ad6bb97a7b13b536791411d21177ab2fc8f7a349
Analese Steverson Pugh <analese_steversonpugh@intuit.com>
2022-08-31 00:29:55 +0000

    added app skeleton

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ charturl = 'https://plot.ly/python/choropleth-maps/'
 sourceurl = 'https://www.kaggle.com/datasets/surajjha101/us-117th-house-of-representatives'
 githublink = 'https://github.com/astever31/data-science-project-8'
 # here's the list of possible columns to choose from.
-list_of_columns = []
+list_of_states = ["Alaska", "Alabama", "Arkansas", "Arizona", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
 
 ########## Set up the chart
 
@@ -25,56 +25,26 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title=tabtitle
 
+########### Set up the chart
+results = df.groupby(['age_range', 'party'])['name'].count()
+results = results.unstack(level=-1)
+results.reset_index(level=0, inplace=True)
+fig = results.plot(x='age_range', y=['Democratic', 'Republican'], kind='bar');
+
+#beer_fig = go.Figure(data=beer_data, layout=beer_layout)
 ########### Set up the layout
 
 app.layout = html.Div(children=[
-    html.H1('117th US House of Representatives'),
-    html.Div([
-        html.Div([
-                html.H6('Select a variable for analysis:'),
-                dcc.Dropdown(
-                    id='options-drop',
-                    options=[{'label': i, 'value': i} for i in df[district]],
-                    value=df[district][0],
-                    optionHeight=50
-                ),
-        ], className='two columns'),
-        html.Div([dcc.Graph(id='figure-1'),
-            ], className='ten columns'),
-    ], className='twelve columns'),
+    html.H1(myheading),
+    dcc.Graph(
+        id='flyingdog',
+        figure=fig
+    ),
     html.A('Code on Github', href=githublink),
     html.Br(),
-    html.A("Chart Source", href=charturl),
-    html.Br(),
-    html.A("Data Source", href=sourceurl),
-]
+    html.A('Data Source', href=sourceurl),
+    ]
 )
-
-
-# make a function that can intake any varname and produce a map.
-@app.callback(Output('figure-1', 'figure'),
-             [Input('options-drop', 'value')])
-def make_figure(varname):
-    mygraphtitle = f'Exports of {varname} in 2022'
-    mycolorscale = 'sunset' # Note: The error message will list possible color scales.
-    mycolorbartitle = "Millions USD"
-
-    data=go.Choropleth(
-        locations=df['test_state'], # Spatial coordinates
-        locationmode = 'USA-states', # set of locations match entries in `locations`
-        z = df[varname].astype(float), # Data to be color-coded
-        colorscale = mycolorscale,
-        colorbar_title = mycolorbartitle,
-    )
-    fig = go.Figure(data)
-    fig.update_layout(
-        title_text = mygraphtitle,
-        geo_scope='usa',
-        width=1200,
-        height=800
-    )
-    return fig
-
 
 ############ Deploy
 if __name__ == '__main__':
